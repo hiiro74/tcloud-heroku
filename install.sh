1309f4bffe14f8ee359e5cb2f1a4b0c7b9447d6a
hiiro74 <35561495+hiiro74@users.noreply.github.com>
2020-06-01 02:06:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,38 +13,38 @@ cd tcloud
 npm install --only=prod
 
 echo "===== Create config.json ====="
-touch /app/tcloud/config.json
-echo '{' >> /app/tcloud/config.json
-echo '  "log": {' >> /app/tcloud/config.json
-echo '    "path": "logs/"' >> /app/tcloud/config.json
-echo '  },' >> /app/tcloud/config.json
-echo '  "server": {' >> /app/tcloud/config.json
-echo '    "port": 5000,' >> /app/tcloud/config.json
-echo '    "masterKey": "mymasterkey",' >> /app/tcloud/config.json
-echo '    "https": false,' >> /app/tcloud/config.json
-echo '    "hostname": "",' >> /app/tcloud/config.json
-echo '    "certs": {' >> /app/tcloud/config.json
-echo '      "privatekey": "",' >> /app/tcloud/config.json
-echo '      "certificate": "",' >> /app/tcloud/config.json
-echo '      "chain": ""' >> /app/tcloud/config.json
-echo '    }' >> /app/tcloud/config.json
-echo '  },' >> /app/tcloud/config.json
-echo '  "database": {' >> /app/tcloud/config.json
-echo '    "path": "database/"' >> /app/tcloud/config.json
-echo '  },' >> /app/tcloud/config.json
-echo '  "authentification": true,' >> /app/tcloud/config.json
-echo '  "registration": true,' >> /app/tcloud/config.json
-echo '  "files": {' >> /app/tcloud/config.json
-echo '    "path": "files/",' >> /app/tcloud/config.json
-echo '    "tmp": ".tmp/"' >> /app/tcloud/config.json
-echo '  },' >> /app/tcloud/config.json
-echo '  "torrent": {' >> /app/tcloud/config.json
-echo '    "providers": [],' >> /app/tcloud/config.json
-echo '    "jackett": {' >> /app/tcloud/config.json
-echo '      "endpoint": "",' >> /app/tcloud/config.json
-echo '      "apiKey": ""' >> /app/tcloud/config.json
-echo '    }' >> /app/tcloud/config.json
-echo '  },' >> /app/tcloud/config.json
-echo '  "adminUser": "adminko"' >> /app/tcloud/config.json
-echo '}' >> /app/tcloud/config.json
-sed -i 's/mymasterkey$/'$(printf '%03d' $((RANDOM%900)) && printf '%03d' $((RANDOM%900)) && printf 'm@ster' && printf '%03d' $((RANDOM%900)) && printf '%03d' $((RANDOM%900)) && printf '%03d' $((RANDOM%900)))'/' /app/tcloud/config.json
+touch config.json
+echo '{' >> config.json
+echo '  "log": {' >> config.json
+echo '    "path": "logs/"' >> config.json
+echo '  },' >> config.json
+echo '  "server": {' >> config.json
+echo '    "port": 5000,' >> config.json
+echo '    "masterKey": "mymasterkey",' >> config.json
+echo '    "https": false,' >> config.json
+echo '    "hostname": "",' >> config.json
+echo '    "certs": {' >> config.json
+echo '      "privatekey": "",' >> config.json
+echo '      "certificate": "",' >> config.json
+echo '      "chain": ""' >> config.json
+echo '    }' >> config.json
+echo '  },' >> config.json
+echo '  "database": {' >> config.json
+echo '    "path": "database/"' >> config.json
+echo '  },' >> config.json
+echo '  "authentification": true,' >> config.json
+echo '  "registration": true,' >> config.json
+echo '  "files": {' >> config.json
+echo '    "path": "files/",' >> config.json
+echo '    "tmp": ".tmp/"' >> config.json
+echo '  },' >> config.json
+echo '  "torrent": {' >> config.json
+echo '    "providers": [],' >> config.json
+echo '    "jackett": {' >> config.json
+echo '      "endpoint": "",' >> config.json
+echo '      "apiKey": ""' >> config.json
+echo '    }' >> config.json
+echo '  },' >> config.json
+echo '  "adminUser": "adminko"' >> config.json
+echo '}' >> config.json
+sed -i 's/mymasterkey$/'$(printf '%03d' $((RANDOM%900)) && printf '%03d' $((RANDOM%900)) && printf 'm@ster' && printf '%03d' $((RANDOM%900)) && printf '%03d' $((RANDOM%900)) && printf '%03d' $((RANDOM%900)))'/' config.json
